1ec841796b5de4d2c1b9d04e5042721013af5f96
Jacky Puso Lephoto <jacky.lephoto@gmail.com>
2024-05-16 15:28:05 +0200

    update products

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -5,10 +5,10 @@ const Cards = ({filteredItems}) => {
   return (
     <div className='grid xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 items-center justify-center gap-12 shadow-sm'>
         {
-            filteredItems.map((item) =>(
+            filteredItems.slice(0, 8).map((item) =>(
                 <div key={item.id}>
                     <Link to={`/shop/${item.id}`}>
-                        <img src={item.image} alt="" />
+                        <img src={item.image} alt="" className='mx-auto w-full hover:scale-105 transition-all duration-300' />
                     </Link>
                     <div className='mt-4 px-4'>
                         <h4 className='text-base font-semibold mb-2'>{item.title}</h4>
--- frontend/src/pages/home/Products.jsx
+++ frontend/src/pages/home/Products.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
 import { FaFilter } from 'react-icons/fa'
 import Cards from '../../components/Cards';
+import Category from './Category';
 
 const Products = () => {
     const[products, setProducts] = useState([]);
+    const [filteredItems, setFilteredItems] = useState([]);
+    const [seletedCategory, setSelectedCategory] = useState("all");
+    const [sortOption, setSortOption] = useState("default");
 
     useState(() => {
         const fetchData = async () => {
@@ -11,13 +15,57 @@ const Products = () => {
                 const respons = await fetch("products.json");
                 const data = await respons.json()
                 setProducts(data)
+                setFilteredItems(data)
             } catch (error) {
                 console.log("Error fetching data:, error")
             }
         }
         fetchData();
     }, [])
-    console.log(products)
+    // console.log(products)
+
+    // filtering function
+    const filterItems = (category) => {
+        const filtered = Category === "all" ? (products) : products.filter((item) => item.category === category);
+
+        setFilteredItems(filtered);
+        setSelectedCategory(category);
+    }
+
+    // show all products
+    const showall = () => {
+        setFilteredItems(products);
+        seletedCategory("all");
+    }
+
+    // sorting fuctionality
+
+    const handleSortChange = (option) => {
+        setSortOption(option);
+
+        // logic for sorting
+        let sortedItems = [...filteredItems];
+        
+        switch (option) {
+            case "A-Z" : 
+                sortedItems.sort((a, b) => a.title.localeCompare(b.title));
+                break;
+            case "Z-A" :
+                sortedItems.sort((a, b) => b.title.localeCompare(a.title));
+                break;
+            case "low-to-high" :
+                sortedItems.sort((a, b) => a.price - b.price);
+                break;
+            case "high-to-low" :
+                sortedItems.sort((a, b) => b.price - a.price);
+                break;
+            default :
+                break;
+        }
+
+        setFilteredItems(sortedItems);
+    }
+
   return (
     <div className='max-w-screen-2xl container mx-auto xl:px-28 px-4 mb-12'>
         <h2 className='title'>Or subscribe to the newsletter</h2>
@@ -27,17 +75,21 @@ const Products = () => {
             <div className='flex flex-col md:flex-row flex-wrap md:justify-between items-center space-y-3 mb-8'>
                 {/* all btn */}
                 <div className='flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap'>
-                    <button>All Products</button>
-                    <button>Clothing</button>
-                    <button>Hoodies</button>
-                    <button>Bags</button>
+                    <button onClick={showall}>All Products</button>
+                    <button onClick={() => filterItems("Dress")}>Clothing</button>
+                    <button onClick={() => filterItems("Hoodies")}>Hoodies</button>
+                    <button onClick={() => filterItems("Bag")}>Bags</button>
                 </div>
                 {/* sorting option */}
                 <div className='flex justify-end mb-4 rounded:sm'>
                     <div className='bg-black p-2'>
                         <FaFilter className='text-white h-4 w-4'/>
                     </div>
-                    <select className='bg-black text-white px-2 py-1 rounded-sm'>
+                    <select 
+                    id='sort'
+                    onChange={(e) => handleSortChange(e.target.value)}
+                    value={sortOption}
+                    className='bg-black text-white px-2 py-1 rounded-sm'>
                         <option value="default">Default</option>
                         <option value="A-Z">A-Z</option>
                         <option value="Z-A">Z-A</option>
@@ -46,7 +98,7 @@ const Products = () => {
                     </select>
                 </div>
             </div>
-            <Cards filteredItems={products}/>
+            <Cards filteredItems={filteredItems}/>
         </div>
     </div>
   )

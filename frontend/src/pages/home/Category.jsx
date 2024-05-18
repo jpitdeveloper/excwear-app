c588c386ef2712b6e0253abd4713bdb3deaa0f84
Jacky Puso Lephoto <jacky.lephoto@gmail.com>
2024-05-18 12:09:46 +0200

    add collection section

--- frontend/src/pages/home/Category.jsx
+++ frontend/src/pages/home/Category.jsx
@@ -12,7 +12,7 @@ const comapanyLogo = [
 const Category = () => {
   return (
     <div className='max-w-screen-2xl mx auto container xl:px-28 px-4 py-28'>
-        {/* brand logo */}
+        {/* brands logo */}
         <div className='flex items-center justify-around flex-wrap gap-4 py-5'>
             {
                 comapanyLogo.map(({id, img}) => (
